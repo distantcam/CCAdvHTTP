ae89063f084b4c806be8927418d9352b05f52c60
Cameron MacFarland <distantcam@gmail.com>
2013-10-22 17:04:41 +0800

    Build system working

---
+++ src/tutorial/generic/GenericBlock.java
@@ -0,0 +1,13 @@
+﻿package tutorial.generic;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+
+public class GenericBlock extends Block
+{
+    public GenericBlock (int id, Material material)
+    {
+        super(id, material);
+    }
+}
